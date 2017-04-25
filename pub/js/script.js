617ce176af4f3e45324ce6d4c19fb5e17e159216
Anna <annie.malysheva@gmail.com>
2017-04-25 11:49:09 +0300

    added event on font sizes list

--- pub/js/script.js
+++ pub/js/script.js
@@ -1,3 +1,13 @@
 /**
  * Created by Anna on 24.04.2017.
  */
+var elem = document.getElementById("fontSize");
+
+elem.addEventListener("click", function() {
+    var list = document.getElementById("fontSizeList");
+    if (list.style.display !== "block") {
+        list.style.display = "block";
+    } else {
+        list.style.display = "none";
+    }
+});
