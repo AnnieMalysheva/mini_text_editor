c64e306e63a6b43fb89050bab1c233ffedae310d
Anna <annie.malysheva@gmail.com>
2017-04-25 14:47:06 +0300

    added dropdowns to buttons

--- pub/js/script.js
+++ pub/js/script.js
@@ -1,9 +1,9 @@
 /**
  * Created by Anna on 24.04.2017.
  */
-var elem = document.getElementById("fontSize");
+var dropdown1 = document.getElementById("fontSize");
 
-elem.addEventListener("click", function() {
+dropdown1.addEventListener("click", function() {
     var list = document.getElementById("fontSizeList");
     if (list.style.display !== "block") {
         list.style.display = "block";
@@ -12,3 +12,13 @@ elem.addEventListener("click", function() {
     }
 });
 
+var dropdown2 = document.getElementById("colorPalette");
+
+dropdown2.addEventListener("click", function () {
+    var table = document.getElementById("colorPicker");
+    if (table.style.display !== "block") {
+        table.style.display = "block";
+    } else {
+        table.style.display = "none";
+    }
+});
