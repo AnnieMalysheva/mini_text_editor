185b6a35d1288fff7ec0c10700dca29e4aa133ba
Anna <annie.malysheva@gmail.com>
2017-04-26 20:34:20 +0300

    added function for several dropdowns

--- pub/js/script.js
+++ pub/js/script.js
@@ -1,24 +1,28 @@
 /**
  * Created by Anna on 24.04.2017.
  */
-var dropdown1 = document.getElementById("fontSize");
+var dropdowns = document.querySelectorAll('[data-type="dropdown"]');
 
-dropdown1.addEventListener("click", function() {
-    var list = document.getElementById("fontSizeList");
-    if (list.style.display !== "block") {
-        list.style.display = "block";
-    } else {
-        list.style.display = "none";
+if (dropdowns) {
+    for (var i = 0; i < dropdowns.length; i++) {
+        dropdowns[i].addEventListener("click", thisDropdown, false)
     }
-});
+}
 
-var dropdown2 = document.getElementById("colorPalette");
-
-dropdown2.addEventListener("click", function () {
-    var table = document.getElementById("colorPicker");
-    if (table.style.display !== "block") {
-        table.style.display = "block";
+function thisDropdown (event) {
+    var targetEl = event.target;
+    var dropElement = targetEl.nextElementSibling;
+    if (dropElement.style.display !== "block") {
+        dropElement.style.display = "block";
     } else {
-        table.style.display = "none";
+        dropElement.style.display = "none";
     }
+}
+
+// selection
+var selectedObject = document.getSelection();
+var setAlignLeaft = document.getElementById("btnAlignLeft");
+
+setAlignLeaft.addEventListener("click", function () {
+    alert(selectedObject);
 });
